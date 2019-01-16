d6c7ea1a70d1f2d1280804498bf4579cc2c593e1
lionhard83 <carloleonardi83@gmail.com>
2019-01-16 09:25:34 +0100

    remove cors

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
 
 var todos = require('./routes/todos');
 app.use('/todos', todos);
